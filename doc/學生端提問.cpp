9ebce1aeeead1484ed4fd919495ed5541eaed5e3
chain0128 <43954853+chain0128@users.noreply.github.com>
2018-12-22 00:06:31 +0800

    Add files via upload

--- doc/學生端提問.cpp
+++ doc/學生端提問.cpp
@@ -15,23 +15,36 @@
 
 int main(){
 	char *question;
-	question=(char*)malloc( 50 * sizeof(char) );
-	int i=0,j=0,k=0,l=0;
+	question=(char*)malloc( 1000 * sizeof(char) );
+	char *answer;
+	answer=(char*)malloc(1000 * sizeof(char) );
 	
+	int i=0,k=0,l=0;
 	
+	FILE *fp2=fopen("test.txt","r");
+	while(!feof(fp2)){
+		fscanf(fp2,"%s",answer);
+	}
+	printf("%s\n",answer);
+
+	fclose(fp2);
 	
 	FILE *fp=fopen("test.txt","a");
 	
-	fprintf(fp,"問題:");
+	
+	printf("要問什麼問題:");
+	fprintf(fp,"要問的問題:");
 	for(i=0;;i++){
 		scanf("%c",&question[i]);
 		fprintf(fp,"%c",question[i]);
 		if(question[i]=='\n'){
-			break;
+			printf("提問成功\");
+			break;	
 		} 
-		else if(i>50){
+		else if(i>1000){
 			question = (char*)realloc(question, i * sizeof(char));
 		}
 	}
+	fclose(fp); 
 }
 	
--- doc/老師端回答.cpp
+++ doc/老師端回答.cpp
@@ -15,23 +15,38 @@
 
 int main(){
 	char *question;
-	question=(char*)malloc( 50 * sizeof(char) );
+	question=(char*)malloc( 1000 * sizeof(char) );
+	char *answer;
+	answer=(char*)malloc(1000 * sizeof(char) );
 	int i=0,j=0,k=0,l=0;
 	
+	FILE *fp=fopen("test.txt","r");
+	while(!feof(fp)){
+		fscanf(fp,"%s",question);
+	
+	}
+	printf("%s\n",question);
+	fclose(fp);
+	
+
+	FILE *fp2=fopen("test.txt","a");
 	
 	
-	FILE *fp=fopen("test.txt","a");
+	
+	printf("請輸入答案:");
 	
 	fprintf(fp,"回答:");
 	for(i=0;;i++){
-		scanf("%c",&question[i]);
+		scanf("%c",&answer[i]);
 		
-		fprintf(fp,"%c",question[i]);
-		if(question[i]=='\n'){
+		fprintf(fp,"%c",answer[i]);
+		if(answer[i]=='\n'){
+			printf("回答成功\");
 			break;
 		} 
 		else if(i>50){
-			question = (char*)realloc(question, i * sizeof(char));
+			answer = (char*)realloc(answer, i * sizeof(char));
 		}
 	}
+	fclose(fp2);
 }
